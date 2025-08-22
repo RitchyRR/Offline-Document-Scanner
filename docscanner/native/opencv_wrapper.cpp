ace7750eb11982505b28800dc07a89dd0fa75f78
rr3069s <richard.raetzer@alumni.fh-aachen.de>
2025-08-22 12:27:35 +0200

    2.1.6+147

--- docscanner/native/opencv_wrapper.cpp
+++ docscanner/native/opencv_wrapper.cpp
@@ -80,7 +80,7 @@ static bool _writeUncompressedPng(
         // Write PNG to temporaray file name
         std::vector<int> compression_params = { 
             cv::IMWRITE_PNG_COMPRESSION, 
-            0 // Compression levels: 0 - 9
+            2 // Compression levels: 0 - 9
         };
         bool success = cv::imwrite(tmpPath, inImage, compression_params);
         if (!success) {
